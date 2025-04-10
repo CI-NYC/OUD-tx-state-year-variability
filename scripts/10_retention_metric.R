b597977c7687f6a7ef7cb134b6459bddba61e5b5
Anton Hung <amh2389@ip-172-25-96-227.ec2.internal>
2025-04-10 18:33:24 +0000

    completed observed metrics

--- scripts/10_retention_metric.R
+++ scripts/10_retention_metric.R
@@ -18,8 +18,8 @@ source("~/medicaid/OUD_tx_state_year_variability/R/helpers.R")
 
 # load cohort and opioid data
 cohort <- load_data("hillary_cohort_with_exclusions.fst", drv_root) |>
-  filter(exclusion_jul4 == 0) |>
-  mutate(follow_up_end_dt = index_dt %m+% days(180))
+  filter(exclusion_jul4 == 0) #|>
+  # mutate(follow_up_end_dt = index_dt %m+% days(180))
 
 bup <- load_data("moud_bup_intervals.fst", drv_root)
 methadone <- load_data("moud_methadone_intervals.fst", drv_root)
@@ -35,12 +35,17 @@ moud <- moud[, .(BENE_ID, moud_start_dt, moud_end_dt)] |>
   distinct()
 
 moud <- moud |>
-  left_join(cohort |> select(BENE_ID, index_dt, follow_up_end_dt)) |>
-  filter(moud_end_dt >= index_dt) #|>
-  # slice(1:10000)
+  left_join(cohort |> select(BENE_ID, index_dt)) |>
+  filter(moud_start_dt >= index_dt)
+
+moud <- moud[, .SD[min(moud_start_dt) <= as.Date("2019-07-05")], by = BENE_ID]
+
+moud_dates <- moud[, .(
+  moud_start_dt = min(moud_start_dt), 
+  follow_up_until_dt = min(moud_start_dt) + days(179)
+), by = BENE_ID]
 
 # Calculate duration -----------------------------------------------------
-
 moud <- moud[, list(data = list(data.table(.SD))), by = BENE_ID]
 
 get_duration <- function(data, gap = 7) {
@@ -53,13 +58,12 @@ get_duration <- function(data, gap = 7) {
     as.data.table()
   
   # all dates with some opioid prescription
-  opioid_dates <- to_modify[, .(date = seq(moud_start_dt, moud_end_dt, by = "1 day"),
-                                follow_up_end_dt), 
+  opioid_dates <- to_modify[, .(date = seq(moud_start_dt, moud_end_dt, by = "1 day")), 
                             by = .(seq_len(nrow(to_modify)))
   ][, seq_len := 1] |> distinct()
   
   # all dates in hypothetical exposure period
-  all_dates_exposure_period <- data[, .(date = seq(index_dt, follow_up_end_dt, by = "1 day")), 
+  all_dates_exposure_period <- data[, .(date = seq(moud_start_dt, moud_start_dt %m+% days(179), by = "1 day")), 
                                     by = .(seq_len(nrow(data)))
   ][seq_len == 1][, seq_len := NULL]
   
@@ -106,7 +110,7 @@ out <- foreach(data = moud$data,
                id = moud$BENE_ID,
                .combine = "rbind",
                .options.future = list(chunk.size = 1e4)) %dofuture% {
-                 out <- get_duration(data, gap = 0)
+                 out <- get_duration(data, gap = 7)
                  out$BENE_ID <- id
                  setcolorder(out, "BENE_ID")
                  out
@@ -114,11 +118,15 @@ out <- foreach(data = moud$data,
 
 plan(sequential)
 
-cohorcot <- cohort |>
-  left_join(out) |>
-  mutate(retention_metric = ifelse(!is.na(follow_up_end_dt) & 
-                                   follow_up_end_dt == exposure_end_dt, 1, 0),
-         retention_metric = ifelse(is.na(follow_up_end_dt), 0, retention_metric))
+result <- out |>
+  left_join(moud_dates)|>
+  mutate(retention_metric = as.numeric(follow_up_until_dt <= exposure_end_dt))
+# 
+# cohort <- cohort |>
+#   left_join(out) |>
+#   mutate(retention_metric = ifelse(!is.na(follow_up_end_dt) & 
+#                                    follow_up_end_dt == exposure_end_dt, 1, 0),
+#          retention_metric = ifelse(is.na(follow_up_end_dt), 0, retention_metric))
 
-write_data(cohort, "retention_metric.fst", drv_root)
+write_data(result, "retention_metric.fst", drv_root)
 
